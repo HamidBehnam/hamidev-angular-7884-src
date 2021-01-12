1116b21c3dc2352e3b4d4982cfad1a0dac676b2e
HamidBehnam <hamid.behnam@gmail.com>
2021-01-12 14:32:07 -0800

    updating the auth setting in the app module

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
   "hamidev-nodejs-8324-src": {
     auth0_domain: "dev-2nbjdzvu.us.auth0.com",
     auth0_client_id: "79qkAdchT3jEEXtwnw1Ot5D0zD3TYYV6",
-    auth0_audience: "https://dev.hamidbehnam.com/nodejs/8324/"
+    auth0_audience: "https://dev.hamidbehnam.com/nodejs/8324/",
+    auth0_redirect_uri: "https://dev.hamidbehnam.com/angular/7884/"
   }
 };
 
