0653742e4bb460d99b8aeec74db55b0f5851e3a0
HamidBehnam <hamid.behnam@gmail.com>
2021-01-12 16:02:26 -0800

    auth test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {AuthService} from '@auth0/auth0-angular';
 import jwt_decode from "jwt-decode";
+import {RedirectLoginOptions} from '@auth0/auth0-spa-js';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,22 @@ export class AppComponent {
   title = 'hamidev-angular-src';
   accessToken: string;
   accessTokenDecoded: any;
+  redirectLoginOptions: RedirectLoginOptions;
 
   constructor(public authService: AuthService) {
     this.authService.user$.subscribe(user => {
       console.log(user);
     });
+
+    console.log(window.location);
+
+    this.redirectLoginOptions = {
+      redirect_uri: window.location.origin,
+      appState: {
+        theName: "Hamid",
+        theCode: 234
+      }
+    };
   }
 
   async getAccessToken() {
