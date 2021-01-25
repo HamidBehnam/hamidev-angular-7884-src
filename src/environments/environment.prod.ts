6aa7ea569fabbeaf11b85fb79a47696e194c838b
HamidBehnam <hamid.behnam@gmail.com>
2021-01-24 22:15:00 -0800

    remove the dropdown

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,10 @@ export const environment = {
     auth0_domain: "dev-2nbjdzvu.us.auth0.com",
     auth0_client_id: "79qkAdchT3jEEXtwnw1Ot5D0zD3TYYV6",
     auth0_audience: "https://dev.hamidbehnam.com/nodejs/8324/"
+  },
+  "hamidev-nodejs-test-src": {
+    auth0_domain: "dev-2nbjdzvu.us.auth0.com",
+    auth0_client_id: "79qkAdchT3jEEXtwnw1Ot5D0zD3TYYV6",
+    auth0_audience: "https://dev.hamidbehnam.com/nodejs/8324/"
   }
 };
